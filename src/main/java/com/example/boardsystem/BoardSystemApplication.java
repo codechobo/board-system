45cbfbca3b35bed252c36a1d84f50f8623adde1b
이경범 <tkdwk567@naver.com>
2022-06-22 16:01:18 +0900

    Initial commit

---
+++ src/main/java/com/example/boardsystem/BoardSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.boardsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardSystemApplication.class, args);
+    }
+
+}
