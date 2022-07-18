7be97012b62af074fe276d5a0c1957af8fbf6414
이경범 <tkdwk567@naver.com>
2022-07-18 16:42:30 +0900

    post 도메인 생성 테스트 리펙토링

--- src/test/java/com/study/boardsystem/domain/PostTest.java
+++ src/test/java/com/study/boardsystem/domain/PostTest.java
@@ -1,13 +1,9 @@
 package com.study.boardsystem.domain;
 
-import com.study.boardsystem.module.user.domain.User;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * packageName    : com.study.boardsystem.domain
@@ -15,38 +11,18 @@ import javax.persistence.PersistenceContext;
  * author         : tkdwk567@naver.com
  * date           : 2022/06/28
  */
-
-
-@SpringBootTest
 class PostTest {
 
-    @PersistenceContext
-    EntityManager em;
-
-    @Transactional
     @Test
-    @DisplayName("게시글 생성")
+    @DisplayName("post 객체 생성")
     void PostTest() {
-        User user = User.builder()
-                .nickname("검정")
-                .password("검정12")
-                .email("검정@naver.com")
-                .name("이름")
-                .city("서울")
-                .address1("어디?")
-                .address2("어딘가?")
-                .build();
-
-        em.persist(user);
-
         Post post = Post.builder()
-                .title("타이틀이다.")
-                .description("글 써라")
+                .userName("스폰지밥")
+                .title("제목")
+                .description("글 내용 길게~~~")
                 .build();
 
-        post.addUser(user);
-        em.persist(post);
-
-
+        assertThat(post).isNotNull();
+        assertThat(post.getTitle()).isEqualTo("제목");
     }
 }
